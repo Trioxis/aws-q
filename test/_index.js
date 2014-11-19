17f9502e4852fef8c4eebac86c8fdd8978390ac9
Jack Davis <mr.jackdavis@gmail.com>
2014-11-19 17:40:32 +1100

    Rough out test to prove this module should inject 'then' into AWS.Request prototype

--- test/_index.js
+++ test/_index.js
@@ -1,7 +1,17 @@
 var expect = require("expect.js");
+var AwsQ = require("../index.js");
 
 describe('entry point',function(){
-	it('should do stuff',function(){
-		
+	it('should inject/hack \'then\' into aws.response.prototype',function(){
+		var AWSMock = {
+			Request:{
+
+			}
+		};
+
+		AwsQ(AWSMock);
+
+		expect(AWSMock.Request).to.have.property("then");
+
 	});
 });
