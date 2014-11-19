e8eeb4ed1c3a699d591e1727821cd2d9026e41e0
Jack Davis <mr.jackdavis@gmail.com>
2014-11-20 09:16:08 +1100

    Expand tests to prove Request.then returns a promise

--- test/_index.js
+++ test/_index.js
@@ -1,13 +1,33 @@
 var expect = require("expect.js");
-var AWS = require('aws-sdk');
 var AwsQ = require("../index.js");
 
-describe('entry point',function(){
+var AwsMock;
+beforeEach(function(){
+	AwsMock = {
+		Request:{
+			prototype:{
+
+			}
+		}
+	};
+});
+
+describe('AwsQ entry point',function(){
 	it('should inject/hack \'then\' into aws.response.prototype',function(){
+		AwsQ.InjectMagic(AwsMock);
+
+		expect(AwsMock.Request.prototype).to.have.property("then");
+		expect(AwsMock.Request.prototype.then).to.be.a('function');
+	});
+});
+
+describe('Aws.Request.prototype.then function',function(){
+	it('should return a Q promise',function(){
+		var AWS = require('aws-sdk');
 		AwsQ.InjectMagic(AWS);
 
-		expect(AWS.Request.prototype).to.have.property("then");
-		expect(AWS.Request.prototype.then).to.be.a('function');
-
+		var ec2 = new AWS.EC2();
+		var promise = ec2.describeAccountAttributes({}).then();
+		expect(promise).to.be.a('promise');
 	});
 });
